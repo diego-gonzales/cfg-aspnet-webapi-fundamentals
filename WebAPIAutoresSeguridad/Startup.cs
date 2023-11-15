7d305d8bd58a8089f781ba07b6f309c38a283304
Diego Gonzales <diegoegj14@gmail.com>
2023-11-15 10:40:11 -0500

    Add Swagger support for API authentication

--- WebAPIAutoresSeguridad/Startup.cs
+++ WebAPIAutoresSeguridad/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace WebAPIAutoresSeguridad;
 
@@ -19,7 +20,43 @@ public class Startup
     public void ConfigurationService(IServiceCollection services)
     {
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc(
+                "v1",
+                new OpenApiInfo() { Title = "Web Api Autores", Version = "v1" }
+            );
+
+            // Configuración para que Swagger pueda autenticarse con JWT
+            options.AddSecurityDefinition(
+                "Bearer",
+                new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                }
+            );
+
+            options.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                }
+            );
+        });
         services
             .AddControllers()
             .AddJsonOptions(
