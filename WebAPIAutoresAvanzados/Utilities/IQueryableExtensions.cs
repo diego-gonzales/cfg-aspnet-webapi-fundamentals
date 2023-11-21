9314734ff2f7d1e1d55fe341c626069998a89d32
Diego Gonzales <diegoegj14@gmail.com>
2023-11-21 17:38:39 -0500

    Extend IQueryable with pagination functionality

--- WebAPIAutoresAvanzados/Utilities/IQueryableExtensions.cs
+++ WebAPIAutoresAvanzados/Utilities/IQueryableExtensions.cs
@@ -3,7 +3,7 @@
 public static class IQueryableExtensions
 {
     public static IQueryable<T> Paginate<T>(
-        this IQueryable<T> queryable,
+        this IQueryable<T> queryable, // IQueryable<T> es una interfaz que permite hacer consultas a la base de datos, 'this' es para extender el IQueryable<T>
         PaginationDTO paginationDTO
     )
     {
